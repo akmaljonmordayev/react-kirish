8c41974df3abda57192ed128bca693996e3c48da
akmaljonmordayev <akmaljonmordayev@gmail.com>
2025-05-28 18:29:44 +0500

    ol added

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,32 @@ function App() {
 
   return (
     <>
-      <h1>
-        Lorem ipsum dolor sit.
-      </h1>
-      <h2>
-        Lorem ipsum dolor sit.
-      </h2>
+      <h1>Lorem ipsum dolor sit.</h1>
+      <h2>Lorem ipsum dolor sit.</h2>
+      <ol>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+        <li>olma</li>
+      </ol>
     </>
-  )
+  );
 }
 
 export default App
